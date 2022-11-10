74b09c055f4cfa073efd7ad23021ba46d9e5445b
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-11-10 12:15:37 +0700

    Order: add screen Header

--- App-onTapTH/src/components/ComponentOrder.js
+++ App-onTapTH/src/components/ComponentOrder.js
@@ -1,6 +1,7 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import IconFontAwesome from 'react-native-vector-icons/FontAwesome'
+import IconMaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import IconMaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 //   <Icon name="rocket" color="#eee" size={30} />
 
@@ -9,7 +10,6 @@ import dbCoffee from '../database/dataLocal'
 
 const ComponentOrder = ({ route, navigation }) => {
     console.log("🚀 ~ file: ComponentOrder.js ~ line 35 ~ ComponentOrder ~ ComponentOrder")
-
 
     //
     const [chooseDeliver, setChooseDeliver] = useState(styles.btnOrder_1);
@@ -27,6 +27,8 @@ const ComponentOrder = ({ route, navigation }) => {
         }
 
     }
+    /// so luong SP
+    let soLuong = 1;
 
 
     // console.log("🚀 ~ file: ComponentOrder.js ~ line 5 ~ ComponentOrder ~ route", route)
@@ -88,6 +90,63 @@ const ComponentOrder = ({ route, navigation }) => {
 
                         }]} >Pick Up</Text>
                     </TouchableOpacity>
+                </View>
+
+                {/*  */}
+                <View   style={[  ,{ }]} >
+                    <Text  style={[  ,{
+                        fontWeight:'bold',
+                        fontSize: 18,
+                        marginTop: 25,
+                    }]} >Deliver Address</Text>
+                    <Text  style={[  ,{
+                        fontWeight: 'bold',
+                        marginTop: 15,
+                    }]} >Jl. Kpg Sutoyo</Text>
+                    <Text  style={[  ,{
+                        marginTop: 8,
+                        opacity: 0.5,
+                        fontSize: 12,
+                    }]} >Kpg. Sutoyo No. 620, Bilzen, Tanjungbalai.</Text>
+                    <View  style={[ styles.row ,{
+                        marginTop: 10,
+                    }]}  >
+                        <Text  style={[  ,{
+                            fontSize:12,
+                            opacity: 0.8,
+                            borderRadius: 10,
+                            borderWidth: 0.5,
+                            padding: 4,
+                            paddingHorizontal: 8,
+                            marginRight: 10,
+                        }]} ><IconFontAwesome name='edit' size={12} color={'#000'} /> Edit Address</Text>
+                        <Text  style={[  ,{
+                            fontSize:12,
+                            opacity: 0.8,
+                            borderRadius: 10,
+                            borderWidth: 0.5,
+                            padding: 4,
+                            paddingHorizontal: 8,
+                            marginRight: 10,
+                        }]} ><IconMaterialCommunityIcons name='note-text-outline' size={12} color={'#000'} /> Add Note</Text>
+                    </View>
+                </View>
+
+                {/*  */}
+                <View   style={[ styles.flex_1, styles.row ,{
+                    marginTop: 20,
+                    borderTopWidth:0.5,
+                    paddingVertical: 15,
+                    alignItems: 'center'
+                }]} >
+                    <Image source={item.img} style ={{width:75, height:75, borderRadius: 15}}/>
+                    <View  style={[ styles.flex_1 ,{marginHorizontal: 10}]} >
+                        <Text  style={[  ,{fontWeight: 'bold'}]} >{item.name}</Text>
+                        <Text  style={[  ,{opacity: 0.5, fontSize: 13, marginTop: 4}]} >with {item.nhaSX}</Text>
+                    </View>
+                    <IconMaterialCommunityIcons name='minus-circle-outline' size={25} color={'#000'}/>
+                    <Text  style={[  ,{padding: 8, fontSize: 16,}]} >{soLuong}</Text>
+                    <IconMaterialIcons name='add-circle-outline' size={25} color={'#000'}/>
                 </View>
             </View>
             <View style={[, {
